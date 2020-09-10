10ba4871129e13dc832eae35a54b565778639683
Mauricio Rojas <mrojasb2000@gmail.com>
2020-09-10 17:48:27 -0300

    Refactor Hello World application and add gitignore file

--- HelloWorld/helloworld.kt
+++ HelloWorld/helloworld.kt
@@ -1,4 +1,11 @@
 val name: String = "Mavro"
+var greeting: String? = null
+
 fun main(args: Array<String>) {
+    greeting = "Hello"
+    when (greeting) {
+        null -> println("Hi")
+        else -> println(greeting)
+    }
     println("Hello World! " + name)
 }
