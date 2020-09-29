ce8f05a91411f7e465044874c587b8ca1c6160e1
Mauricio Rojas <mrojasb2000@gmail.com>
2020-09-29 16:36:43 -0300

    Add path variables

--- Microservices/chapter2/src/main/kotlin/com/microservices/chapter2/controller/CustomerController.kt
+++ Microservices/chapter2/src/main/kotlin/com/microservices/chapter2/controller/CustomerController.kt
@@ -2,6 +2,7 @@ package com.microservices.chapter2.controller
 
 import com.microservices.chapter2.model.Customer
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,6 @@ class CustomerController {
     @Autowired
     lateinit var customers : ConcurrentHashMap<Int, Customer>
 
-    @RequestMapping(value = ["/customer"], method = [RequestMethod.GET])
-    fun getCustomer() = customers[2]
+    @RequestMapping(value = ["/customer/{id}"], method = [RequestMethod.GET])
+    fun getCustomer(@PathVariable id: Int) = customers[id]
 }
