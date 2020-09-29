fdfacb3e3cd75c61f4ad6d7a723138f6763bf714
Mauricio Rojas <mrojasb2000@gmail.com>
2020-09-29 16:58:25 -0300

    Add request parameters

--- Microservices/chapter2/src/main/kotlin/com/microservices/chapter2/controller/CustomerController.kt
+++ Microservices/chapter2/src/main/kotlin/com/microservices/chapter2/controller/CustomerController.kt
@@ -2,10 +2,7 @@ package com.microservices.chapter2.controller
 
 import com.microservices.chapter2.model.Customer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
 @RestController
@@ -17,5 +14,8 @@ class CustomerController {
     fun getCustomer(@PathVariable id: Int) = customers[id]
 
     @RequestMapping(value = ["/customers"], method = [RequestMethod.GET])
-    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
+    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String)
+            = customers.filter {
+                it.value.name.contains(nameFilter, true)
+            }.map(Map.Entry<Int, Customer>::value).toList()
 }
