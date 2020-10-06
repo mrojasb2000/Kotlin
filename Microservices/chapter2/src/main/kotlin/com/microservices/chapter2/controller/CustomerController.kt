ba337a0466b41d51f84a4a8f7b35a5f442b33e53
Mauricio Rojas <mrojasb2000@gmail.com>
2020-10-06 16:55:15 -0300

    Refactor status response by methods

--- Microservices/chapter2/src/main/kotlin/com/microservices/chapter2/controller/CustomerController.kt
+++ Microservices/chapter2/src/main/kotlin/com/microservices/chapter2/controller/CustomerController.kt
@@ -13,27 +13,40 @@ class CustomerController {
     lateinit var customerService : CustomerService
 
     @GetMapping(value = ["/customer/{id}"])
-    fun getCustomer(@PathVariable id: Int) : ResponseEntity<Customer>? {
+    fun getCustomer(@PathVariable id: Int) : ResponseEntity<Customer?> {
         val customer = customerService.getCustomer(id)
         val status = if (customer == null) HttpStatus.NOT_FOUND else HttpStatus.OK
         return   ResponseEntity(customer, status)
     }
-
 
     @GetMapping(value = ["/customers"])
     fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String)
             = customerService.searchCustomers(nameFilter)
 
     @PostMapping(value = ["/customer/"])
-    fun createCustomer(@RequestBody customer: Customer) {
+    fun createCustomer(@RequestBody customer: Customer) : ResponseEntity<Unit>{
         customerService.createCustomer(customer)
+        return ResponseEntity(Unit, HttpStatus.CREATED)
     }
 
     @DeleteMapping(value = ["/customer/{id}"])
-    fun deleteCustomer(@PathVariable id: Int) = customerService.deleteCustomer(id)
+    fun deleteCustomer(@PathVariable id: Int) : ResponseEntity<Unit> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.deleteCustomer(id)
+            status = HttpStatus.OK
+        }
+        return ResponseEntity(Unit, status)
+    }
 
     @PutMapping(value = ["/customer/{id}"])
-    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer){
-        customerService.updateCustomer(id, customer)
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer) : ResponseEntity<Unit>{
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.updateCustomer(id, customer)
+            status = HttpStatus.ACCEPTED
+        }
+        return ResponseEntity(Unit, status)
+
     }
 }
